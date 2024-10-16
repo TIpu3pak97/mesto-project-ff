1404d6377947e768fc3e6b27e56bf507b061f505
Ivan Kashirin <kashiriniaec@yandex.ru>
2024-10-16 20:55:50 +0500

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+const content = document.querySelector('.content');
+const cardList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function createCard(cardTitle, cardImage, deletedCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImg = cardElement.querySelector(".card__image");
+    cardElement.querySelector('.card__title').textContent = cardTitle;
+    cardImg.alt = cardTitle;
+    cardImg.src = cardImage;
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+        deleteButton.addEventListener('click', (evt) => {
+            deletedCard(evt);
+        });
+        return cardElement;
+};
 
-// @todo: Функция удаления карточки
+const deleteCard = (event) => {
+    const item = event.target.closest('.card');
+    item.remove();
+};
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function ({name, link}) {
+    const cardData = createCard(name, link, deleteCard);
+    cardList.append(cardData);
+});
